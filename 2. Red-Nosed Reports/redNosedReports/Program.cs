0e6ec2cbfaa5be12250ee0b8b7da52e8c8dc4153
aob123 <alexobeswick@gmail.com>
2024-12-13 00:39:19 +0100

    updated StreamReader

--- 2. Red-Nosed Reports/redNosedReports/Program.cs
+++ 2. Red-Nosed Reports/redNosedReports/Program.cs
@@ -1,1 +1,12 @@
 ﻿
+
+using (StreamReader sr = new StreamReader("/Users/alexanderbeswick/Documents/Dev/advent_of_code/2. Red-Nosed Reports/redNosedReports/input.txt"))
+{
+    string line;
+
+    while ((line = sr.ReadLine()) != null)
+    {
+        string[] items = line.Split(" ");
+        System.Console.WriteLine(items[0]); 
+    }
+}
--- 2. Red-Nosed Reports/redNosedReports/obj/Debug/net9.0/redNosedReports.AssemblyInfo.cs
+++ 2. Red-Nosed Reports/redNosedReports/obj/Debug/net9.0/redNosedReports.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("redNosedReports")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ce313d92bfa9c9132ded61083cee77b637ca4da")]
 [assembly: System.Reflection.AssemblyProductAttribute("redNosedReports")]
 [assembly: System.Reflection.AssemblyTitleAttribute("redNosedReports")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
